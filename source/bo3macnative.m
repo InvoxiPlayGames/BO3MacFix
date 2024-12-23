29eaa3fa82f9594f5d0e2f17d6b3f381fa211d11
InvoxiPlayGames <webmaster@invoxiplaygames.uk>
2024-12-23 02:33:52 +0000

    join the dark side

--- source/bo3macnative.m
+++ source/bo3macnative.m
@@ -8,5 +8,8 @@ void set_window_title(const char *new_title) {
     strncpy(new_title_buf, new_title, sizeof(new_title_buf));
     dispatch_async(dispatch_get_main_queue(), ^(void) {
         [[[NSApplication sharedApplication] mainWindow] setTitle:[NSString stringWithUTF8String:new_title_buf]];
+        if (@available(macOS 10.14, *)) {
+            [NSApplication sharedApplication].appearance = [NSAppearance appearanceNamed:NSAppearanceNameDarkAqua];
+        }
     });
 }
